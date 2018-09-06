2dadbcb723433d77fe01dc7ece96df3251794621
Gabriele Comoretto <gcomoretto@lsst.org>
2018-09-06 14:54:21 -0700

    Updated how the apr package is found

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,5 +1,4 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-APR_DIR="$LSST_CONDA_ENV_NAME/include/apr-1"
 
-CONFIGURE_OPTIONS="--prefix=$PREFIX --with-apr=$APR_DIR"
+CONFIGURE_OPTIONS="--prefix=$PREFIX --with-apr=$(command -v apr-1-config)"
