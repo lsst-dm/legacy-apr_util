7cd713fb5faf4f417427c33b513438ee547f2011
Gabriele Comoretto <gcomoretto@lsst.org>
2018-09-06 14:13:46 -0700

    Added APR_DIR definition

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,3 +1,6 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-CONFIGURE_OPTIONS="--prefix=$PREFIX"
+export APR_DIR="$LSST_CONDA_ENV_NAME/include/apr-1"
+
+
+CONFIGURE_OPTIONS="--prefix=$PREFIX --with-apr=$APR_DIR"
