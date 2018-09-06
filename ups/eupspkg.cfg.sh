68a2935ecf9af3f3025e9b1b986b559c45d028c5
Gabriele Comoretto <gcomoretto@lsst.org>
2018-09-06 14:16:25 -0700

    Removed export

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,6 +1,5 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-export APR_DIR="$LSST_CONDA_ENV_NAME/include/apr-1"
-
+APR_DIR="$LSST_CONDA_ENV_NAME/include/apr-1"
 
 CONFIGURE_OPTIONS="--prefix=$PREFIX --with-apr=$APR_DIR"
