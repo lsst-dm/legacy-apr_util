772da744b0248030d92f2023359d1a0296967284
Gabriele Comoretto <gcomoretto@lsst.org>
2018-09-07 10:49:09 -0700

    Reset PATH when configuring
    This will avoid that when compiling apr_util, libraries from
    local anaconda3 installation are used, for example iconv

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,3 +1,14 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-CONFIGURE_OPTIONS="--prefix=$PREFIX --with-apr=$APR_DIR"
+config()
+{
+
+    #
+    # PATH reset during configuration
+    # in order that anaconda3 packages are not found 
+    # (for example iconv)
+    #
+    export PATH="/bin:/usr/bin/:/usr/local/bin"
+    ./configure --prefix="$PREFIX" --with-apr="$APR_DIR"
+
+}
