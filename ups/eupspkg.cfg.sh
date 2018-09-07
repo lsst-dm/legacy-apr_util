7bfc2dfa0754df6dc98ead7219de5bf99d001a17
Gabriele Comoretto <gcomoretto@lsst.org>
2018-09-07 10:23:11 -0700

    Added confi section

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,5 +1,10 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
-export PATH="/bin:/usr/bin/:/usr/local/bin"
 
-CONFIGURE_OPTIONS="--prefix=$PREFIX --with-apr=$APR_DIR"
+config()
+{
+
+    export PATH="/bin:/usr/bin/:/usr/local/bin"
+    ./configure --prefix="$PREFIX" --with-apr="$APR_DIR"
+
+}
